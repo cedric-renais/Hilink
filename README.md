ba623bb7027d918772ed7092dd79f8333a145e22
Cédric RENAIS <cedric.renais@gmail.com>
2024-09-02 14:37:53 +0200

    🎉 Début du projet : Hilink ⚡ Tech Stack : Next, Typescript & Tailwind 📦️ Dépendances : postcss, prettier & prettier-plugin-tailwindcss ⚙️ Configuration du theme Tailwind personnalisé ♿ WCAG : Choix des couleurs pour assurer un score de contraste AAA ⭐ Ajout du favicon

---
+++ README.md
@@ -0,0 +1,31 @@
+<div align="center">
+  <br />
+
+# Projet Hilink
+
+  <br />
+</div>
+
+## 🎉 Introduction
+
+Ce projet a pour objectif de créer une interface web moderne et dynamique pour une application de voyages et d'aventures. En utilisant Tailwind CSS, je vais explorer les capacités de ce framework pour concevoir un design réactif et élégant.
+
+## 🎯 Objectif
+
+- <b>Apprendre Tailwind</b> : Approfondir les connaissances en utilisant les classes utilitaires pour créer des mises en page flexibles et performantes.
+
+- <b>Optimiser l'Expérience Utilisateur</b> : Assurer une interface fluide et engageante pour les visiteurs, avec une attention particulière à l'accessibilité.
+
+## ⚡ Tech Stack
+
+- Next JS
+- Typescript
+- Tailwind CSS
+
+## 📦️ Dépendances
+
+- postcss
+- prettier
+- prettier-plugin-tailwindcss
+
+#
---
+++ app/page.tsx
@@ -0,0 +1,46 @@
+export default function Home() {
+  return (
+    <div className="mt-32 flex h-[100vh] flex-col gap-10">
+      <div>
+        <h1 className="text-center text-3xl font-bold text-gray-9">
+          Ceci est un essai de contraste pour texte large
+        </h1>
+        <p className="mb-6 text-center text-gray-9">
+          Ceci est un essai de contraste pour texte normal
+        </p>
+      </div>
+      <div>
+        <h2 className="text-center text-3xl font-bold text-green-9">
+          Ceci est un essai de contraste pour texte large
+        </h2>
+        <p className="mb-6 text-center text-green-9">
+          Ceci est un essai de contraste pour texte normal
+        </p>
+      </div>
+      <div>
+        <h2 className="text-center text-3xl font-bold text-red-9">
+          Ceci est un essai de contraste pour texte large
+        </h2>
+        <p className="mb-6 text-center text-red-9">
+          Ceci est un essai de contraste pour texte normal
+        </p>
+      </div>
+      <div>
+        <h2 className="text-center text-3xl font-bold text-blue-9">
+          Ceci est un essai de contraste pour texte large
+        </h2>
+        <p className="mb-6 text-center text-blue-9">
+          Ceci est un essai de contraste pour texte normal
+        </p>
+      </div>
+      <div>
+        <h2 className="text-purple-9 text-center text-3xl font-bold">
+          Ceci est un essai de contraste pour texte large
+        </h2>
+        <p className="text-purple-9 mb-6 text-center">
+          Ceci est un essai de contraste pour texte normal
+        </p>
+      </div>
+    </div>
+  );
+}
